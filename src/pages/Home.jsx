97548be7d37c01aa1e04318b8e8be1b3e788bcf1
SaiNaveen4478 <vusasainaveen@gmail.com>
2026-01-12 16:06:53 +0530

    udate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,12 +9,12 @@ export default function Home() {
       <div className="overlay">
         <div className="content">
           <h1>
-            Event Management <span>Platform</span>
+            Eventtify <span></span>
           </h1>
 
           <p>
-            A modern web application to create, manage and book events
-            seamlessly with a professional user experience.
+           Our Event Management Platform makes it easy to explore upcoming events, book tickets securely, and manage your bookings all in one place. From tech conferences and workshops to music nights and festivals, experience seamless event planning with a modern and intuitive interface.
+            track bookings, and stay organized — all designed to deliver a smooth and engaging user experience.
           </p>
 
           {!isLoggedIn ? (
